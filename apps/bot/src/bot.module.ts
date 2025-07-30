7749b31a009dc5f28b3b631c8bff3f0a3004bb70
Lev Roskoshin <lev.roskoshin@myexlab.com>
2025-07-30 21:15:15 +0200

    feat: add queue

--- apps/bot/src/bot.module.ts
+++ apps/bot/src/bot.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { ConfigService } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { Env, EnvModule, PrismaModule } from '@app/shared';
@@ -8,6 +9,14 @@ import { BotUpdate } from './bot.update';
   imports: [
     EnvModule,
     PrismaModule,
+    BullModule.forRootAsync({
+      useFactory: (configService: ConfigService<Env>) => ({
+        connection: {
+          url: configService.get('REDIS_URL', { infer: true })!,
+        },
+      }),
+      inject: [ConfigService],
+    }),
     TelegrafModule.forRootAsync({
       useFactory: (configService: ConfigService<Env>) => ({
         token: configService.get('BOT_TOKEN', { infer: true })!,
